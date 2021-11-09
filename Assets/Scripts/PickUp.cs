e994b21202d513fb52c002b44789841b7ca27830
Hjallimar <37866588+Hjallimar@users.noreply.github.com>
2021-11-09 11:25:37 +0100

    Added fun stuff for the pick up

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem OnCollect;
+    [SerializeField] private CircleCollider2D Collider;
+    [SerializeField] private List<SpriteRenderer> Sprites;
+    private float value = 0.5f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            OnCollect.Play();
+            Collider.enabled = false;
+            foreach (SpriteRenderer sprite in Sprites)
+            {
+                sprite.enabled = false;
+            }
             Debug.Log("Shieet dawn I got collected");
+            
+        }
+    }
+
+    void Update()
+    {
+        Vector2 TransChange = new Vector2(value, 0.0f);
+        transform.localScale -= (Vector3)TransChange * Time.deltaTime;
+        if (transform.localScale.x < 0.2f)
+        {
+            value *= -1;
+            transform.localScale = new Vector3(0.2f, 1,1);
+        }
+        else if (transform.localScale.x > 1.0f)
+        {
+            transform.localScale = new Vector3(1f, 1,1);
+            value *= -1;
         }
     }
 }
+
