f850fb1159db295aa02219ed94d6329c396ee6f6
DESKTOP-9B6JMC2\aboud <aboud-malki@hotmail.com>
2021-11-10 11:58:05 +0100

    Added gravity projectile

--- Assets/Aboud Workspace/Scripts/Gravity/Projectile.cs
+++ Assets/Aboud Workspace/Scripts/Gravity/Projectile.cs
@@ -1,25 +1,25 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    public Vector3 initialVelocity;
-    public Vector3 velocity;
-    public Vector3 gravity;
-
+    public  Vector3          velocity;
+    public  Vector3          gravity;
+    public  List<GameObject> transformPoints;
     
+    private Vector3 currentTarget;
+    private int     currentIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        initialVelocity = velocity;
+        currentTarget = transformPoints[currentIndex].transform.position - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += velocity *Time.deltaTime;
-        velocity += gravity * Time.deltaTime;
+        transform.position += (velocity+currentTarget) * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -29,6 +29,13 @@ public class Projectile : MonoBehaviour
             gravity = (other.transform.position - transform.position)*0.05f;
             gravity.x = 0;
             gravity.z = 0;
+        }
+        
+        if (other.CompareTag("transTarget"))
+        {
+            currentIndex++;
+            if(currentIndex < transformPoints.Count)
+                currentTarget = transformPoints[currentIndex].transform.position - transform.position;
         }
     }
 
