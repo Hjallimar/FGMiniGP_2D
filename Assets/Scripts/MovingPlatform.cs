00e627e621917cf8b70df5a165673cfd6f8bddef
Hjallimar <37866588+Hjallimar@users.noreply.github.com>
2021-11-10 12:01:40 +0100

    Added a moving platform

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D MyRigidBody;
+    [SerializeField] private Transform EndLocation;
+    [SerializeField, Range(1.0f, 10.0f)] private float TravelTime = 5.0f;
+    [SerializeField, Range(0.0f, 10.0f)] private float WaitTime = 5.0f;
+    private Vector2 StartPos;
+    private Vector2 EndPos;
+    private float LerpValue;
+    private int Direction = 1;
+
+    private bool Travel = true;
+    private bool Wait = false;
+    
+    private float TimeCounter = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRigidBody.isKinematic = true;
+        MyRigidBody.gravityScale = 0.0f;
+        StartPos = transform.position;
+        EndPos = EndLocation.position;
+        TimeCounter = 0.0f;
+        LerpValue = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateNewValue();
+        transform.position = Vector2.Lerp(StartPos, EndPos, LerpValue);
+        Debug.Log("Lerp: " + LerpValue + ", Timer :" + TimeCounter + ", Direction:" + Direction);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.gameObject.transform.SetParent(transform);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other.gameObject.transform.SetParent(null);
+    }
+
+    void CalculateNewValue()
+    {
+        TimeCounter += Time.deltaTime;
+        if (Travel)
+        {
+            if(Direction > 0)
+                LerpValue = TimeCounter / TravelTime;
+            else
+                LerpValue = 1.0f - (TimeCounter / TravelTime);
+            if (TimeCounter >= TravelTime)
+            {
+                Travel = false;
+                TimeCounter = 0.0f;
+                Direction *= -1;
+                Wait = true;
+            }
+        }
+        else if (Wait)
+        {
+            if (TimeCounter >= WaitTime)
+            {
+                TimeCounter = 0.0f;
+                Travel = true;
+                Wait = false;
+            }
+        }
+    }
+}
