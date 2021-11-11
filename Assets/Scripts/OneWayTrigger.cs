857264a7190be87eaecd063b70f65afce64bf3af
Hjallimar <37866588+Hjallimar@users.noreply.github.com>
2021-11-11 15:32:31 +0100

    One way trigger fixed??

--- Assets/Scripts/OneWayTrigger.cs
+++ Assets/Scripts/OneWayTrigger.cs
@@ -1,17 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class OneWayTrigger : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D Collider;
-
+    private Vector3 point;
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !other.isTrigger)
         {
-            Collider.isTrigger = false;
+            Vector3 Diff = other.gameObject.transform.position - transform.position;
+            point = Diff.normalized;
+            float Allign = Vector3.Dot(Diff.normalized, transform.up);
+            Debug.Log(Allign + " is current dot, values are " + Diff.normalized +  ":" + transform.up);
+            if (Allign > 0)
+            {
+                Collider.isTrigger = false;
+            }
         }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.up * 10.0f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + point * 10.0f);
     }
 }
