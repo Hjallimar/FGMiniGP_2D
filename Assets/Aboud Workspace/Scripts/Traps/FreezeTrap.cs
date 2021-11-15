c40743e8ffe85eebee64be2ddc1baf05fb969b85
DESKTOP-9B6JMC2\aboud <aboud-malki@hotmail.com>
2021-11-15 11:19:55 +0100

    Added Capturing ropes to freezetrap

--- Assets/Aboud Workspace/Scripts/Traps/FreezeTrap.cs
+++ Assets/Aboud Workspace/Scripts/Traps/FreezeTrap.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 
 public class FreezeTrap : MonoBehaviour
 {
-    [SerializeField] private float freezeDuration;
+    [SerializeField] private float        freezeDuration;
+    [SerializeField] private                  GameObject[] ropeShooters;
 
     private PlayerController controller;
     private Rigidbody2D      rigidBody;
     private Vector3          freezePosition;
     private bool             needsReset;
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !needsReset)
         {
             controller = other.GetComponentInParent<PlayerController>();
             controller.enabled = false;
+            
             rigidBody = other.GetComponent<Rigidbody2D>();
             rigidBody.velocity = Vector2.zero;
             rigidBody.gravityScale = 0;
+            
             needsReset = true;
-            StartCoroutine("FreezePlayer");
+            
+            
+            for (int i = 0; i < ropeShooters.Length; i++)
+            {
+                ropeShooters[i].GetComponent<LineRenderer>().SetVertexCount(2);
+                ropeShooters[i].GetComponent<LineRenderer>().SetPosition(0, ropeShooters[i].transform.position);
+                ropeShooters[i].GetComponent<LineRenderer>().SetPosition(1, transform.position);
+            }
+            
+            StartCoroutine("UnFreezePlayer");
         }
     }
 
@@ -33,10 +45,17 @@ public class FreezeTrap : MonoBehaviour
         }
     }
 
-    private IEnumerator FreezePlayer()
+    private IEnumerator UnFreezePlayer()
     {
         yield return new WaitForSeconds(freezeDuration);
         rigidBody.gravityScale = 1;
+        
         controller.enabled = true;
+        
+        for (int i = 0; i < ropeShooters.Length; i++)
+        {
+            ropeShooters[i].GetComponent<LineRenderer>().SetVertexCount(0);
+            
+        }
     }
 }
